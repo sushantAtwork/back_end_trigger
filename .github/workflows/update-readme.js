87df3fb5f4efd27d33239f62d8d6549caf5e4153
sushantAtwork <144010690+sushantAtwork@users.noreply.github.com>
2023-09-07 11:26:14 +0530

    Update update-readme.js

--- .github/workflows/update-readme.js
+++ .github/workflows/update-readme.js
@@ -1,27 +1,26 @@
 const fs = require('fs').promises;
 const { Octokit } = require('@octokit/rest');
 
-// GitHub API credentials (personal access token)
 const octokit = new Octokit({
   auth: process.env.TEST_REPO,
 });
 
 async function generateTableRows() {
   try {
-    // Fetch branches from your repository
+
     const { data: branches } = await octokit.request('GET /repos/{owner}/{repo}/branches', {
       owner: 'sushantAtwork',
       repo: 'front-end-trigger',
     });
 
-    // Fetch open PRs from your repository
+
     const { data: prs } = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
       owner: 'sushantAtwork',
       repo: 'front-end-trigger',
       state: 'open',
     });
 
-    // Generate Markdown table rows based on branch and PR information
+
     const tableRows = [];
     
     branches.forEach((branch) => {
@@ -41,18 +40,11 @@ async function generateTableRows() {
 
 async function updateReadmeTable() {
   try {
-    // Read the existing README.md content using fs.promises.readFile
+
     let readmeContent = await fs.readFile('README.md', 'utf8');
-
-    // Generate the dynamic table rows
     const dynamicRows = await generateTableRows();
-
-    // Replace the <!-- TABLE_ROWS --> placeholder with the dynamic rows
     readmeContent = readmeContent.replace('<!-- TABLE_ROWS -->', dynamicRows);
-
-    // Write the updated content back to README.md using fs.promises.writeFile
     await fs.writeFile('README.md', readmeContent);
-
     console.log('README.md table updated successfully.');
   } catch (error) {
     console.error('Error:', error.message);
